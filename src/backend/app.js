c59ae796f19b4ab372a7f045b53b90af19130542
Angel <aboahangel16@gmail.com>
2024-02-23 16:28:04 +0100

    change implemented

--- src/backend/app.js
+++ src/backend/app.js
@@ -7,7 +7,7 @@ const mealsRouter = require("./api/meals");
 const buildPath = path.join(__dirname, "../../dist");
 const port = process.env.PORT || 3000;
 const cors = require("cors");
-const knex = require("knex");
+const knex = require("./database");
 
 // For week4 no need to look into this!
 // Serve the built client html
