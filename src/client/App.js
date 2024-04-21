f7c26263b07b5c5c759f8b8058ac7737bdff009b
Angel <aboahangel16@gmail.com>
2024-04-21 09:12:49 +0200

    updated App.js file

--- src/client/App.js
+++ src/client/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 import Header from "./components/Header/Header";
 import { Meals } from "./components/MealsPage/Meals";
 import { MealWithId } from "./components/MealById/MealWithId";
